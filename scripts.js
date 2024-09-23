9efdf592fbbd2e195d622edf2ad3697110393e93
wil-tec-74 <ordosalas3@gmail.com>
2024-09-22 22:17:14 -0400

    museo

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('startBtn').addEventListener('click', function() {
+    document.getElementById('welcome').classList.add('d-none');
+    document.getElementById('computer-parts').classList.remove('d-none');
+  });
+  
